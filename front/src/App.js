4e04b0a3e8f1c8db8481c91c03edd50e502d7e0c
Benoît Hubert <benoithubert@gmail.com>
2018-06-19 08:03:11 +0200

    Google maps integration

--- front/src/App.js
+++ front/src/App.js
@@ -1,18 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+import { apiKey } from './apiKey'
+
+const MyMapComponent = withScriptjs(withGoogleMap((props) =>
+  <GoogleMap
+    defaultZoom={12}
+    defaultCenter={{ lat: 43.6033385, lng: 1.4525834 }}
+  >
+  {
+    props.markers.map((marker) => <Marker key={marker.id} position={{ lat: marker.lat, lng: marker.lng }} />)
+  }
+  </GoogleMap>
+))
+
 
 class App extends Component {
+  state = {
+    markers: [
+      { id: 1, lat: 43.6033385, lng: 1.4525834 },
+      { id: 2, lat: 43.5736221, lng: 1.3631478 }
+    ]
+  }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <MyMapComponent
+          isMarkerShown
+          markers={this.state.markers}
+          googleMapURL={`https://maps.googleapis.com/maps/api/js?key=${apiKey}&v=3.exp&libraries=geometry,drawing,places`}
+          loadingElement={<div style={{ height: `100%` }} />}
+          containerElement={<div style={{ height: `400px` }} />}
+          mapElement={<div style={{ height: `100%` }} />}
+        />
       </div>
     );
   }
